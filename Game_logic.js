e785b6d3b527de36affa5ced83556ddf1a15d063
lNelnel <113882838+lNelnel@users.noreply.github.com>
2023-03-15 23:22:17 +0100

    Update Game_logic.js

--- Game_logic.js
+++ Game_logic.js
@@ -1,31 +1,64 @@
 class position{
-  x = 0; 
-  y = 0;
+  #x; // position x
+  #y; // position y
 }
-
-
 class WorldCell {
   #obstructed = false;
   #bug = null;
   #food = 0;
-  #marker = null;
+  #marker = {x:0, y:0, i}; // set the marker i at position (x,y)
   #base = null;
+  #position = {x:0, y: 0};
+  constructor(x, y) {
+    this.#position.x = x;
+    this.#position.y = y;
+  }
 
-  isObstructed() = false;
-  isOccupied() = false;
-  getBug() = null;
-  removeBug() = false;
-  
-  setFood(amount){
+  isObstructed() {
+    return this.#obstructed != this.null;
+  }
+  isOccupied() {
+    return this.#bug !== this.null;
+  }
+  getBug() {
+    if (this.#bug !== null) {
+      return new position(this.#bug.x, this.#bug.y);
+    }
+    else
+      return null;
+  }
+  removeBug() {
+    if (this.#bug !== null) {
+      this.#bug = null;
+      new position(this.#bug.x, this.#bug.y);
+      return true;
+    }
+    else
+      return false;
+  }
+
+  setFood(amount) {
     this.#food = amount;
   }
-  
-  isFriendlyBase(color){
-    return #this.#base === color;
+
+  isFriendlyBase(color) {
+    return this.#base === color;
   }
-   isFriendlyBase(color){
-    return #this.#base === color;
+  isFriendlyBase(color) {
+    return this.#base === color;
   }
-  
-  setMarker(color)
+  setMarker(color, i) {
+    if (this.#bug !== null && this.#base === color) {
+      this.#marker = new marker();
+      this.#marker.i = markerIndex;
+    }
+  }
+  clearMarker(color, i) {
+    //this.position
+    if (this.#bug !== null && this.#base === color) {
+      this.#marker = null;;
+      this.#marker.i = 0;
+    }
+  }
+
 }
